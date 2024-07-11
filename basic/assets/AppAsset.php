9662b7f5768a1350531ad7574243a2df85c358e9
taichaubeready <tai.chau@beready.academy>
2024-07-11 21:08:33 +0700

    Finish IT-4552 & Redo IT-4544 - Created Report Alpine JS - Installed Alpine JS module in project Yii 2.0 1. Ignore folder node_modules in .gitignore 2. Disabled boostrap in AppAsset.php 3. Created & Edit tailwind.config.js 4. Install Packages - concurrently , live-server & postcss-cli 5. Added 5 scripts in package.json 6. Tested TailwindCSS & AlpineJS

--- basic/assets/AppAsset.php
+++ basic/assets/AppAsset.php
@@ -23,9 +23,10 @@ class AppAsset extends AssetBundle
         'css/tw.css',
     ];
     public $js = [
+        'js/alpine.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap5\BootstrapAsset'
+        // 'yii\bootstrap5\BootstrapAsset'
     ];
 }
--- basic/views/test-tailwind/index.php
+++ basic/views/test-tailwind/index.php
@@ -23,12 +23,14 @@ AppAsset::register($this);
 
     <!-- Content -->
     <div class="container-fluid" id="content">
-        <h1 class="text-green-600">Nhat Tai - Test Tailwind CSS</h1>
+        <h1 class="text-green-600 dark:bg-gray-400 dark:text-white">Nhat Tai - Test Tailwind CSS</h1>
 
         <p class="text-yellow-600">
             You may change the content of this page by modifying
             the file <code><?= __FILE__; ?></code>.
         </p>
+
+        <h3 class="text-red-600" x-data="{ text: 'Hi This is AlpineJS'}" x-text="text"></h3>
     </div>
 
     <?php $this->endBody() ?>
